a2de11e3522810608f93b39d133c48906ba37433
James Moore <jameswmoore99@gmail.com>
2021-07-31 10:06:57 -0600

    Finished basic wire frame for home ~ Added classes in homeStyling ~ Added material ui components in Home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import useStyles from './HomeStyles.jsx';
 
 const Home = () => {
   const classes = useStyles();
   return (
     <Grid container className={classes.homeContainer}>
-      <Paper>
-        HOME
-      </Paper>
+      <Grid item xs={12} md={6} className={classes.homeContainer}>
+        <Typography variant="h1">
+          James Moore
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={6} className={classes.tabs}>
+        <Typography variant="h1">
+          Area For Tabs
+        </Typography>
+      </Grid>
+      <Grid container className={classes.aboutMe}>
+        <Grid item xs={12} md={8} className={classes.aboutMeParagraph}>
+          <Typography variant="h1" align="left">
+            About me section
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={4} className={classes.aboutMeImg}>
+          <Typography variant="h1" align="left">
+            IMG
+          </Typography>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
--- client/src/components/home/HomeStyles.jsx
+++ client/src/components/home/HomeStyles.jsx
@@ -3,6 +3,19 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(() => ({
   homeContainer: {
     border: '2px solid black'
+  },
+  tabs: {
+    border: '2px solid black'
+  },
+  aboutMe: {
+    border: '2px solid black',
+    height: '80vh'
+  },
+  aboutMeParagraph: {
+    border: '2px solid black'
+  },
+  aboutMeImg: {
+    border: '2px solid black'
   }
 }));
 
