7690be82a9b76390529173d5c24fef4ce72447bd
James Moore <jameswmoore99@gmail.com>
2024-04-30 13:20:17 -0600

    ~ Added personal projects data

--- portfolio/app/sections/expAndProj/projects.tsx
+++ portfolio/app/sections/expAndProj/projects.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import ProjectBlock from "../../customComps/projectBlock";
+import PersonalProjects from "./personalProjcts";
 
 const Projects = () => {
   return (
-    <div className="mt-5">
-      <ProjectBlock
-        title="Wordle"
-        dates={["03/24", "05/24"]}
-        desc="My version of the well-known word game Wordle. You have six attempts to guess the secret word. Yellow letters indicate the correct letter but in the wrong position, while green letters signify both the correct letter and its correct position."
-        feats={["TBD", "TEST", "Will find out"]}
-        tags={["React", "TailwindCSS"]}
-        extraStyling="w-full"
-      />
+    <div>
+      {PersonalProjects.map((job, i) => (
+        <div className="mt-5" key={i}>
+          <div className="flex justify-between">
+            <span className="text-xl text-slate-300">{job.workTitle}</span>
+          </div>
+          <div className="flex flex-col">
+            {job.projects.map((project, j) => (
+              <ProjectBlock
+                key={j}
+                title={project.title}
+                dates={project.dates}
+                desc={project.desc}
+                feats={project.feats}
+                tags={project.stackTags}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
