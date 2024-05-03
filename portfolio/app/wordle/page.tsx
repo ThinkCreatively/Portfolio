1aabe4f74f3ef7979380ab7660d659050e81e2e8
James Moore <jameswmoore99@gmail.com>
2024-05-03 14:43:18 -0600

    ~ Got skeleton for wordles set up

--- portfolio/app/wordle/page.tsx
+++ portfolio/app/wordle/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Wordle = () => {
   return (
-    <div className="flex flex-col justify-center content-between min-h-screen px-6 bg-indigo-950 overflow-hidden lg:flex-row">
-      hello world
+    <div className="flex flex-col justify-center content-between min-h-screen px-6 bg-indigo-950 overflow-hidden">
+      <div className="flex flex-col border-2 min-h-screen justify-center items-center">
+        <div className="border-2 w-96 h-96 mb-5">Test</div>
+        <div className="border-2 w-1/2 h-60">Test</div>
+      </div>
     </div>
   );
 };
