6c00c467e1ee8e268ff690aaf46e7f0597df2edc
James Moore <jameswmoore99@gmail.com>
2024-05-09 21:39:07 -0600

    ~ made exp and projects tabs sticky essentially

--- portfolio/app/sections/pAndE.tsx
+++ portfolio/app/sections/pAndE.tsx
@@ -7,20 +7,34 @@ const PAndE = () => {
   const [expIsActive, setExpIsActive] = useState(true);
 
   return (
-    <div className="flex flex-col overflow-auto h-auto lg:my-20 lg:h-screen">
-      <div className="flex space-x-2">
-        <span className={`text-2xl cursor-pointer ${expIsActive ? "" : "text-gray-500"}`} onClick={() => {
-          setExpIsActive(true);
-        }}>
-          Experience
-        </span>
-        <span className={`text-2xl cursor-pointer ${expIsActive ? "text-gray-500" : ""}`} onClick={() => {
-          setExpIsActive(false);
-        }}>
-          Projects
-        </span>
+    <div className="flex flex-col overflow-auto h-auto lg:mb-20 lg:mt-20 lg:h-screen">
+      <div className="flex flex-col overflow-hidden h-auto py-2 mb-5">
+        <div className="space-x-2">
+          <span
+            className={`text-2xl cursor-pointer ${
+              expIsActive ? "" : "text-gray-500"
+            }`}
+            onClick={() => {
+              setExpIsActive(true);
+            }}
+          >
+            Experience
+          </span>
+          <span
+            className={`text-2xl cursor-pointer ${
+              expIsActive ? "text-gray-500" : ""
+            }`}
+            onClick={() => {
+              setExpIsActive(false);
+            }}
+          >
+            Projects
+          </span>
+        </div>
       </div>
-      {expIsActive ? <Experience /> : <Projects />}
+      <div className="flex flex-col overflow-auto h-auto lg:h-screen">
+        {expIsActive ? <Experience /> : <Projects />}
+      </div>
     </div>
   );
 };
