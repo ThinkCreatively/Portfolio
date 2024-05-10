ec6728173fbff4a94bc27756f097021c2624ecb5
James Moore <jameswmoore99@gmail.com>
2024-05-09 20:46:01 -0600

    ~ Finished building out academic section

---
+++ portfolio/app/sections/academicExp.tsx
@@ -0,0 +1,39 @@
+const AcademicExperience = [
+  {
+    workTitle: "Academics",
+    timeWorked: "",
+    projects: [
+      {
+        title: "Western Governors University",
+        dates: ["01/23", "12/24"],
+        desc: "Bachelors of Science",
+        feats: [
+          "Developed multiple microservices using Java to enhance comprehension of microservice architecture",
+          "Excelled in querying both nonrelational and relational databases, leveraging table joins and query planning to enhance performance",
+          "Acquired proficiency in Python and Flask to develop backend APIs facilitating seamless communication with databases and efficient transmission of data to frontend web applications",
+        ],
+        stackTags: ["Python", "Java", "MongoDB", "MySQL"],
+      },
+      {
+        title: "Hack Reactor",
+        dates: ["04/21", "09/21"],
+        desc: "Bootcamp Certificate",
+        feats: [
+          "Collaborated within a team, adhering to agile methodologies, to develop a Shopify-like application featuring product showcases and their respective reviews",
+          "Managed a database housing millions of entries, enhancing query efficiency by 12% through the strategic utilization of indexes",
+          "Spearheaded the establishment of the foundational framework for a food recipe application, enabling users to seamlessly search for recipes",
+        ],
+        stackTags: [
+          "Javascript",
+          "React",
+          "Express",
+          "Node.js",
+          "MongoDB",
+          "PostgreSQL",
+        ],
+      },
+    ],
+  },
+];
+
+export default AcademicExperience;
